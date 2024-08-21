f30448eb542bd5639fe5d8acc53fd7420ce09097
Brian Wang <brianwang7102@gmail.com>
2024-08-21 19:36:02 -0400

    refactor: changed error response messages

--- utils/error.js
+++ utils/error.js
@@ -1,20 +1,20 @@
 module.exports.Error500 = (res,err) => {
   res.status(500);
   res.send({
-    message: err.message
+    message: "An error has occurred on the server"
   });
 }
 
 module.exports.Error400 = (res, err) => {
   res.status(400);
   res.send({
-    message: err.message
+    message: "Invaid data"
   });
 }
 
 module.exports.Error404 = (res, err) => {
   res.status(404);
   res.send({
-    message: err.message
+    message: "Resource not found"
   });
 }
