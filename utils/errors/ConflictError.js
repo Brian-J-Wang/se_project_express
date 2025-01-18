dd6deab710c4da615c19a7dcc9c6c079e37ef78f
Brian Wang <brianwang7102@gmail.com>
2025-01-17 19:27:39 -0500

    fix: file naming

--- utils/errors/ConflictError.js
+++ utils/errors/ConflictError.js
@@ -1,6 +1,6 @@
 class ConflictError extends Error {
   constructor(message) {
-    super(message ?? "Conflict Error");
+    super(message ?? "Conflicting Error");
     this.statusCode = 409;
   }
 }
